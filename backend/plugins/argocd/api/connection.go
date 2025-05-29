215525b40d3679cfa9a0ad0206accf2ee34d205d
Zvika Badalov <zvika.badalov@finally.com>
2025-05-29 19:04:35 -0400

    feat(argocd): implement connection testing and validation for ArgoCD API

--- backend/plugins/argocd/api/connection.go
+++ backend/plugins/argocd/api/connection.go
@@ -1,20 +1,73 @@
 package api
 
 import (
+	"context"
+	"net/http"
+
 	"github.com/apache/incubator-devlake/core/errors"
 	"github.com/apache/incubator-devlake/core/plugin"
+	"github.com/apache/incubator-devlake/helpers/pluginhelper/api"
+	"github.com/apache/incubator-devlake/plugins/argocd/models"
 )
 
-// Test connection
-func TestConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	// Simplified test - just return success for now
-	return &plugin.ApiResourceOutput{Body: map[string]interface{}{"success": true}}, nil
+func testConnection(ctx context.Context, connection models.ArgoCDConnection) (*plugin.ApiResourceOutput, errors.Error) {
+	// Create API client from connection
+	apiClient, err := api.NewApiClientFromConnection(ctx, basicRes, &connection)
+	if err != nil {
+		return nil, err
+	}
+
+	// Test connection by making a simple API call to ArgoCD
+	// Try to get the version information or list applications
+	res, err := apiClient.Get("api/version", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	switch res.StatusCode {
+	case http.StatusOK:
+		return &plugin.ApiResourceOutput{
+			Body: map[string]interface{}{
+				"success": true,
+				"message": "Connection test successful",
+			},
+			Status: http.StatusOK,
+		}, nil
+	case http.StatusUnauthorized:
+		return nil, errors.HttpStatus(http.StatusBadRequest).New("Authentication failed. Please check your token.")
+	case http.StatusForbidden:
+		return nil, errors.HttpStatus(http.StatusBadRequest).New("Access denied. Please check your permissions.")
+	default:
+		return nil, errors.HttpStatus(http.StatusBadRequest).New("Failed to connect to ArgoCD server")
+	}
 }
 
-// Test existing connection
+// TestConnection tests ArgoCD connection
+func TestConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
+	var connection models.ArgoCDConnection
+	if err := api.Decode(input.Body, &connection, vld); err != nil {
+		return nil, err
+	}
+
+	result, err := testConnection(context.TODO(), connection)
+	if err != nil {
+		return nil, plugin.WrapTestConnectionErrResp(basicRes, err)
+	}
+	return result, nil
+}
+
+// TestExistingConnection tests existing ArgoCD connection
 func TestExistingConnection(input *plugin.ApiResourceInput) (*plugin.ApiResourceOutput, errors.Error) {
-	// Simplified test - just return success for now
-	return &plugin.ApiResourceOutput{Body: map[string]interface{}{"success": true}}, nil
+	connection, err := dsHelper.ConnApi.GetMergedConnection(input)
+	if err != nil {
+		return nil, errors.BadInput.Wrap(err, "can't read connection from database")
+	}
+
+	result, err := testConnection(context.TODO(), *connection)
+	if err != nil {
+		return nil, plugin.WrapTestConnectionErrResp(basicRes, err)
+	}
+	return result, nil
 }
 
 // Connection API handlers
--- backend/plugins/argocd/api/init.go
+++ backend/plugins/argocd/api/init.go
@@ -4,11 +4,16 @@ import (
 	"github.com/apache/incubator-devlake/core/context"
 	helper "github.com/apache/incubator-devlake/helpers/pluginhelper/api"
 	"github.com/apache/incubator-devlake/plugins/argocd/models"
+	"github.com/go-playground/validator/v10"
 )
 
 var dsHelper *helper.DsHelper[models.ArgoCDConnection, models.ArgoCDApplication, models.ArgoCDScopeConfig]
+var vld *validator.Validate
+var basicRes context.BasicRes
 
 func Init(br context.BasicRes, p any) {
+	basicRes = br
+	vld = validator.New()
 	dsHelper = helper.NewDataSourceHelper[
 		models.ArgoCDConnection, models.ArgoCDApplication, models.ArgoCDScopeConfig,
 	](
